12a6c62be9b70ccb20bc7a4290ccd5c84be1133c
John Bayly <6079360+picturelink@users.noreply.github.com>
2021-03-24 10:00:59 +0000

    enhancement - fix integration test
    @aarr0n mentioned in #141 that the integration tests were failing.
    This fixes the test so that it expects a string[] from YArgs, and
    passes as string[] to Barrelsby

--- test/src.ts
+++ test/src.ts
@@ -15,64 +15,67 @@ const console = require("better-console");
 getArgs();
 const location = join(__dirname, "./");
 Promise.all(
-  readdirSync(location)
-    .map(name => join(location, name))
-    .filter(path => lstatSync(path).isDirectory())
-    .map(directory => {
-      const args = Yargs.parse(["--config", join(directory, "barrelsby.json")]);
-      args.directory = join(directory, args.directory as string);
-      return copy(join(directory, "input"), join(directory, "output")).then(
-        () => {
-          Barrelsby(args as any);
-          console.log(`Running integration test in directory ${directory}`);
-          const outputDirectory = join(directory, "output");
-          const expectedDirectory = join(directory, "expected");
-          console.log("Output directory:", outputDirectory);
-          console.log("Expected directory:", expectedDirectory);
-          const comparison = dirCompare.compareSync(
-            outputDirectory,
-            expectedDirectory,
-            {
-              compareContent: true,
-              compareFileAsync:
-                dirCompare.fileCompareHandlers.lineBasedFileCompare
-                  .compareAsync,
-              compareFileSync:
-                dirCompare.fileCompareHandlers.lineBasedFileCompare.compareSync,
-              ignoreLineEnding: true
-            }
-          );
-          if (comparison.differences && comparison.diffSet) {
-            comparison.diffSet
-              .filter(diff => diff.state !== "equal")
-              .map(diff => {
-                const state = ({
-                  distinct: "<>",
-                  equal: "==",
-                  left: "->",
-                  right: "<-"
-                } as any)[diff.state];
-                const name1 = diff.name1 ? diff.name1 : "";
-                const name2 = diff.name2 ? diff.name2 : "";
-                console.error(
-                  `${name1} ${diff.type1} ${state} ${name2} ${diff.type2}`
-                );
-              });
-          }
-          if (comparison.differences && comparison.diffSet) {
-            console.error(
-              `Error: ${comparison.differences} differences found!`
-            );
-          } else {
-            console.info(`No differences found in ${comparison.equalFiles}`);
-          }
-          console.log();
-          return comparison.differences;
-        }
-      );
-    })
+	readdirSync(location)
+		.map(name => join(location, name))
+		.filter(path => lstatSync(path).isDirectory())
+		.map(directory => {
+			const args = Yargs.parse(["--config", join(directory, "barrelsby.json")]);
+			// args.directory is expected to be a string array, so combine the first directory in the test config
+			// with the current directory, as pass that back as an array
+			const firstDir = (args.directory as string[])[0];
+			args.directory = [join(directory, firstDir)];
+			return copy(join(directory, "input"), join(directory, "output")).then(
+				() => {
+					Barrelsby(args as any);
+					console.log(`Running integration test in directory ${directory}`);
+					const outputDirectory = join(directory, "output");
+					const expectedDirectory = join(directory, "expected");
+					console.log("Output directory:", outputDirectory);
+					console.log("Expected directory:", expectedDirectory);
+					const comparison = dirCompare.compareSync(
+						outputDirectory,
+						expectedDirectory,
+						{
+							compareContent: true,
+							compareFileAsync:
+								dirCompare.fileCompareHandlers.lineBasedFileCompare
+									.compareAsync,
+							compareFileSync:
+								dirCompare.fileCompareHandlers.lineBasedFileCompare.compareSync,
+							ignoreLineEnding: true
+						}
+					);
+					if (comparison.differences && comparison.diffSet) {
+						comparison.diffSet
+							.filter(diff => diff.state !== "equal")
+							.map(diff => {
+								const state = ({
+									distinct: "<>",
+									equal: "==",
+									left: "->",
+									right: "<-"
+								} as any)[diff.state];
+								const name1 = diff.name1 ? diff.name1 : "";
+								const name2 = diff.name2 ? diff.name2 : "";
+								console.error(
+									`${name1} ${diff.type1} ${state} ${name2} ${diff.type2}`
+								);
+							});
+					}
+					if (comparison.differences && comparison.diffSet) {
+						console.error(
+							`Error: ${comparison.differences} differences found!`
+						);
+					} else {
+						console.info(`No differences found in ${comparison.equalFiles}`);
+					}
+					console.log();
+					return comparison.differences;
+				}
+			);
+		})
 ).then(differences =>
-  process.exit(
-    differences.filter(differenceCount => differenceCount > 0).length
-  )
+	process.exit(
+		differences.filter(differenceCount => differenceCount > 0).length
+	)
 );
