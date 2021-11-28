c7d53f4563d171c547755045f0d40402f51b1231
Noah Kovacs <kovacsn66@gmail.com>
2021-11-27 23:22:16 -0500

    :recycle: make BuildBarrel.ts a function instead of class, add .prettierrc

--- src/builder.ts
+++ src/builder.ts
@@ -1,19 +1,16 @@
-import path from "path";
+import path from 'path';
 
-import { buildFileSystemBarrel } from "./builders/fileSystem";
-import { buildFlatBarrel } from "./builders/flat";
-import { BaseUrl } from "./options/baseUrl";
-import { Logger } from "./options/logger";
-import { SemicolonCharacter } from "./options/noSemicolon";
-import { StructureOption } from "./options/options";
-import { QuoteCharacter } from "./options/quoteCharacter";
-import {
-  convertPathSeparator,
-  thisDirectory,
-} from "./utilities";
-import { BuildBarrel } from "./tasks/BuildBarrel";
-import {Directory} from "./interfaces/directory.interface";
-import {FileTreeLocation} from "./interfaces/location.interface";
+import { buildFileSystemBarrel } from './builders/fileSystem';
+import { buildFlatBarrel } from './builders/flat';
+import { BaseUrl } from './options/baseUrl';
+import { Logger } from './options/logger';
+import { SemicolonCharacter } from './options/noSemicolon';
+import { StructureOption } from './options/options';
+import { QuoteCharacter } from './options/quoteCharacter';
+import { convertPathSeparator, thisDirectory } from './utilities';
+import { BuildBarrel } from './tasks/BuildBarrel';
+import { Directory } from './interfaces/directory.interface';
+import { FileTreeLocation } from './interfaces/location.interface';
 
 export class Builder {
   private readonly params;
@@ -37,31 +34,30 @@ export class Builder {
     let builder: BarrelBuilder;
     switch (this.params.structure) {
       default:
-      case "flat":
+      case 'flat':
         builder = buildFlatBarrel;
         break;
-      case "filesystem":
+      case 'filesystem':
         builder = buildFileSystemBarrel;
         break;
     }
 
     try {
       // Build the barrels.
-      this.params?.destinations?.forEach(
-        (destination: Directory) =>
-          new BuildBarrel(
-            destination,
-            builder,
-            this.params.quoteCharacter,
-            this.params.semicolonCharacter,
-            this.params.barrelName,
-            this.params.logger,
-            this.params.baseUrl,
-            this.params.exportDefault,
-            this.params.local,
-            this.params.include,
-            this.params.exclude
-          )
+      this.params?.destinations?.forEach((destination: Directory) =>
+        BuildBarrel({
+          directory: destination,
+          builder,
+          quoteCharacter: this.params.quoteCharacter,
+          semicolonCharacter: this.params.semicolonCharacter,
+          barrelName: this.params.barrelName,
+          logger: this.params.logger,
+          baseUrl: this.params.baseUrl,
+          exportDefault: this.params.exportDefault,
+          local: this.params.local,
+          include: this.params.include,
+          exclude: this.params.exclude,
+        })
       );
     } catch (e) {
       // tslint:disable-next-line:no-console
@@ -81,17 +77,13 @@ export type BarrelBuilder = (
 ) => string;
 
 /** Builds the TypeScript */
-export function buildImportPath(
-  directory: Directory,
-  target: FileTreeLocation,
-  baseUrl: BaseUrl
-): string {
+export function buildImportPath(directory: Directory, target: FileTreeLocation, baseUrl: BaseUrl): string {
   // If the base URL option is set then imports should be relative to there.
   const startLocation = baseUrl ? baseUrl : directory.path;
   const relativePath = path.relative(startLocation, target.path);
   // Get the route and ensure it's relative
   let directoryPath = path.dirname(relativePath);
-  if (directoryPath !== ".") {
+  if (directoryPath !== '.') {
     directoryPath = `.${path.sep}${directoryPath}`;
   }
   // Strip off the .ts or .tsx from the file name.
@@ -103,14 +95,14 @@ export function buildImportPath(
 }
 
 function stripThisDirectory(location: string, baseUrl: BaseUrl) {
-  return baseUrl ? location.replace(thisDirectory, "") : location;
+  return baseUrl ? location.replace(thisDirectory, '') : location;
 }
 
 /** Strips the .ts or .tsx file extension from a path and returns the base filename. */
 export function getBasename(relativePath: string) {
-  const mayBeSuffix = [".ts", ".tsx", ".d.ts"];
+  const mayBeSuffix = ['.ts', '.tsx', '.d.ts'];
   let mayBePath = relativePath;
-  mayBeSuffix.map((suffix) => {
+  mayBeSuffix.map(suffix => {
     const tmpPath = path.basename(relativePath, suffix);
     if (tmpPath.length < mayBePath.length) {
       mayBePath = tmpPath;
--- src/tasks/BuildBarrel.ts
+++ src/tasks/BuildBarrel.ts
@@ -1,57 +1,67 @@
-import {convertPathSeparator} from "../utilities";
-import {QuoteCharacter} from "../options/quoteCharacter";
-import {SemicolonCharacter} from "../options/noSemicolon";
-import {Logger} from "../options/logger";
-import {BaseUrl} from "../options/baseUrl";
-import {loadDirectoryModules} from "../modules";
-import path from "path";
-import {addHeaderPrefix} from "../builders/header";
-import fs from "fs";
-import {BarrelBuilder} from "../builder";
-import {Directory} from "../interfaces/directory.interface";
+import { convertPathSeparator } from '../utilities';
+import { QuoteCharacter } from '../options/quoteCharacter';
+import { SemicolonCharacter } from '../options/noSemicolon';
+import { Logger } from '../options/logger';
+import { BaseUrl } from '../options/baseUrl';
+import { loadDirectoryModules } from '../modules';
+import path from 'path';
+import { addHeaderPrefix } from '../builders/header';
+import fs from 'fs';
+import { BarrelBuilder } from '../builder';
+import { Directory } from '../interfaces/directory.interface';
 
-export class BuildBarrel {
-    constructor (
-        directory: Directory,
-        builder: BarrelBuilder,
-        quoteCharacter: QuoteCharacter,
-        semicolonCharacter: SemicolonCharacter,
-        barrelName: string,
-        logger: Logger,
-        baseUrl: BaseUrl,
-        exportDefault: boolean,
-        local: boolean,
-        include: string[],
-        exclude: string[]
-    ) {
-        logger.debug(`Building barrel @ ${directory.path}`);
-        const content = builder(
-            directory,
-            loadDirectoryModules(directory, logger, include, exclude, local),
-            quoteCharacter,
-            semicolonCharacter,
-            logger,
-            baseUrl,
-            exportDefault
-        );
-        const destination = path.join(directory.path, barrelName);
-        if (content.length === 0) {
-            // Skip empty barrels.
-            return;
-        }
-        // Add the header
-        const contentWithHeader = addHeaderPrefix(content);
-        fs.writeFileSync(destination, contentWithHeader);
-        // Update the file tree model with the new barrel.
-        if (!directory.files.some((file) => file.name === barrelName)) {
-            const convertedPath = convertPathSeparator(destination);
-            const barrel = {
-                name: barrelName,
-                path: convertedPath,
-            };
-            logger.debug(`Updating model barrel @ ${convertedPath}`);
-            directory.files.push(barrel);
-            directory.barrel = barrel;
-        }
-    }
-}
+export const BuildBarrel = ({
+  directory,
+  builder,
+  quoteCharacter,
+  semicolonCharacter,
+  barrelName,
+  logger,
+  baseUrl,
+  exportDefault,
+  local,
+  include,
+  exclude,
+}: {
+  directory: Directory;
+  builder: BarrelBuilder;
+  quoteCharacter: QuoteCharacter;
+  semicolonCharacter: SemicolonCharacter;
+  barrelName: string;
+  logger: Logger;
+  baseUrl: BaseUrl;
+  exportDefault: boolean;
+  local: boolean;
+  include: string[];
+  exclude: string[];
+}) => {
+  logger.debug(`Building barrel @ ${directory.path}`);
+  const content = builder(
+    directory,
+    loadDirectoryModules(directory, logger, include, exclude, local),
+    quoteCharacter,
+    semicolonCharacter,
+    logger,
+    baseUrl,
+    exportDefault
+  );
+  const destination = path.join(directory.path, barrelName);
+  if (content.length === 0) {
+    // Skip empty barrels.
+    return;
+  }
+  // Add the header
+  const contentWithHeader = addHeaderPrefix(content);
+  fs.writeFileSync(destination, contentWithHeader);
+  // Update the file tree model with the new barrel.
+  if (!directory.files.some(file => file.name === barrelName)) {
+    const convertedPath = convertPathSeparator(destination);
+    const barrel = {
+      name: barrelName,
+      path: convertedPath,
+    };
+    logger.debug(`Updating model barrel @ ${convertedPath}`);
+    directory.files.push(barrel);
+    directory.barrel = barrel;
+  }
+};
